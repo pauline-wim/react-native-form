2306bb70003ee845854fc235d3e34e0d5092249b
pauline-wim <linette.wim@hotmail.com>
2022-03-02 14:08:37 +0100

    Create form

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+// import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+  Switch,
+} from "react-native";
 
 export default function App() {
+  // const [toggle, setToogle] = useState(false);
+
+  // const handleToggle = () => {
+  //   setToogle((prev) => !prev);
+  // };
+
+  const handleSubmit = () => {
+    console.log("Submit");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Form</Text>
+        <TextInput style={styles.input} placeholder="Your email" />
+        <TextInput
+          style={styles.input}
+          placeholder="Your password"
+          secureTextEntry
+        />
+        {/* <Switch value={toggle} onValueChange={handleToggle} /> */}
+        <TouchableOpacity style={styles.btn} onPress={handleSubmit}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 40,
+  },
+  input: {
+    marginTop: 10,
+    borderWidth: 1,
+    width: 200,
+    padding: 5,
+    borderRadius: 5,
+    borderColor: "lightgrey",
+  },
+  btn: {
+    marginTop: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 5,
   },
 });
